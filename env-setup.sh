75ad3c2a29785661b21addc628a9ca156b0a2a47
Edwin <edwintang10@gmail.com>
2020-05-31 17:00:36 +0800

    Add .zshrc file and modify env-setup script to symlink file

--- env-setup.sh
+++ env-setup.sh
@@ -4,7 +4,13 @@ home_dir=~/
 proj_dir=$home_dir/dotfiles
 
 echo "Setting up your environment...\n"
+
+echo "Linking .zshrc file\n"
+ln -s $proj_dir/.zshrc $home_dir/
+
+echo "\nLink done\n"
 echo "Linking .vim directory...\n"
 
 ln -s $proj_dir/.vim $home_dir/
 echo "\nLink done"
+
