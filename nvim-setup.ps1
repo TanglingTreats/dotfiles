272076d46e4e2e4a2ed1cf6bea9d6224857a1b1d
Edwin Tang <edwintang10@gmail.com>
2023-03-04 23:41:54 +0800

    Add nvim setup powershell script

---
+++ nvim-setup.ps1
@@ -0,0 +1,71 @@
+function Test-ReparsePoint([string]$path) 
+{
+  $target = Get-Item $path -Force -ea SilentlyContinue
+  return [bool]($target.Attributes -band [IO.FileAttributes]::ReparsePoint)
+}
+
+function Convert-Decision([string]$decision)
+{
+	if($decision -match "[y]")
+	{
+		return $true
+	}
+	else
+	{
+		return $false
+	}
+}
+
+$target_dir = "$HOME\AppData\Local\"
+$target_folder = "nvim"
+
+$base_src_dir = (Get-Item .).FullName
+$src_dir = Join-Path -Path $base_src_dir -ChildPath "config"
+$src_dir = Join-Path -Path $src_dir -ChildPath "nvim"
+
+# Check if folder exists
+$folder_exists = $false
+Get-ChildItem $target_dir | Foreach-Object {
+	#Write-Output $_.Name
+	if ($_.Name -eq $target_folder)
+	{
+		$folder_exists = $true
+		$folder_is_symlink = Test-ReparsePoint(Join-Path -Path $target_dir -ChildPath $_)
+	}
+}
+
+if ($folder_exists)
+{
+	Write-Output "Folder exists"
+	$folder_exists = $true
+	if($folder_is_symlink)
+	{
+		Write-Output "Folder exists as a symlink!"
+	}
+	else
+	{
+		$decision = Read-Host -Prompt "Do you want to remove the folder to create a symlink? Warning: This will remove everything within the folder`n"
+		$decision = Convert-Decision($decision)
+
+		if($decision) {
+			$target = Join-Path -Path $target_dir -ChildPath $target_folder
+			Write-Output "Deleting $target"
+			Remove-Item -Recurse -Force $target
+		}
+	}
+}
+else
+{
+	Write-Output "nvim folder does not exist, creating symlink"
+}
+
+if ($decision -or -not($folder_exists))
+{
+	New-Item -ItemType SymbolicLink -Path (Join-Path -Path $target_dir -ChildPath $target_folder) -Target $src_dir
+	Write-Output "Symlink created"
+}
+
+Write-Output $base_src_dir
+Write-Output $src_dir
+Write-Output $target_dir
+
