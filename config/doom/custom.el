e28cfce537375b2310b62dc17719aae11e703036
Edwin Tang <edwintang10@gmail.com>
2024-03-13 22:19:36 +0800

    feat(config): add doom configs

---
+++ config/doom/custom.el
@@ -0,0 +1,13 @@
+(put 'customize-option 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(display-line-numbers-type 'relative))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
