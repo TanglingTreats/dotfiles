bb2e811e43892f06201f943e9ecf25665c33bb7c
Edwin Tang <edwintang10@gmail.com>
2024-03-31 14:19:34 +0800

    style: transparency is true for theme.

--- config/nvim/after/plugin/gruvbox.rc.lua
+++ config/nvim/after/plugin/gruvbox.rc.lua
@@ -22,7 +22,7 @@ gruvbox.setup({
   palette_overrides = {},
   overrides = {},
   dim_inactive = false,
-  transparent_mode = false,
+  transparent_mode = true,
 })
 
 vim.cmd([[colorscheme gruvbox]])
