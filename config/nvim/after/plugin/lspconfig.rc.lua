e4e1fb1d550d7ea9e944b5443d4f0c61e497c5cf
Edwin Tang <edwintang10@gmail.com>
2024-04-13 20:52:15 +0800

    feat(sig): add mapping for signature help and getting token information on hover.

--- config/nvim/after/plugin/lspconfig.rc.lua
+++ config/nvim/after/plugin/lspconfig.rc.lua
@@ -19,6 +19,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts)
     vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
     vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 
     --[[ insert mode ]]
     vim.keymap.set("n", "<C-Space>", vim.lsp.buf.completion, opts)
