2854c297d000cca0f7812a1f3105262b7d2615c8
TanglingTreats <tanglingtreats69@gmail.com>
2024-05-02 23:49:04 +0800

    feat: add mason config

---
+++ config/nvim/after/plugin/mason.rc.lua
@@ -0,0 +1,18 @@
+local status, mason = pcall(require, "mason")
+if not status then return end
+
+mason.setup({
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗"
+    }
+  }
+})
+
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    'jdtls'
+  }
+})
