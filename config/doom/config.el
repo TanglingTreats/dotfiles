4a6e0a1da52cbd6ee54d97668607ea9db44304d8
TanglingTreats <foolstacked@gmail.com>
2025-01-15 23:57:32 +0800

    feat: update doom emacs config.

--- config/doom/config.el
+++ config/doom/config.el
@@ -7,7 +7,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Edwin Tang"
-      user-mail-address "edwintang10@gmail.com")
+      user-mail-address "foolstacked@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
