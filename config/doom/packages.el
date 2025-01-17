627f3d9d029357926ccbe3f0c5b817ede5d76a82
TanglingTreats <foolstacked@gmail.com>
2025-01-18 01:02:45 +0800

    feat: add org mode text packages for doom emacs.

--- config/doom/packages.el
+++ config/doom/packages.el
@@ -48,3 +48,7 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+(package! pangu-spacing)
+;;(unpin! org-roam)
+(package! org-roam-ui
+  :recipe (:host github :repo "org-roam/org-roam-ui"))
