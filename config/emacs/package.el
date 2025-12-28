69ef3306013ac5ca2e4834dd1fa84a2958028b01
TanglingTreats <foolstacked@gmail.com>
2025-12-28 23:01:24 +0800

    feat(emacs): add package file. add multiple cursors

---
+++ config/emacs/package.el
@@ -0,0 +1,8 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+(if (not (package-installed-p 'use-package))
+(package-install 'use-package))
+(setq use-package-always-ensure t)
+(require 'use-package)
+
