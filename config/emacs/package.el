212e554b18d2b2d6469e45b86aaad91158fa56e1
Treats <treats@MrToot@foolstacked.com>
2026-01-11 22:11:26 +0800

    chore: change path of alias. add package to emacs.

--- config/emacs/package.el
+++ config/emacs/package.el
@@ -3,9 +3,12 @@
 (package-initialize)
 
 (if (not (package-installed-p 'use-package))
-(package-install 'use-package))
+    (package-install 'use-package))
 (setq use-package-always-ensure t)
 (require 'use-package)
 
 (if (not (package-installed-p 'multiple-cursors))
-(package-install 'multiple-cursors))
+    (package-install 'multiple-cursors))
+
+(if (not (package-installed-p 'magit))
+    (package-install 'magit))
